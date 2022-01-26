dd87232cf86c7d4a7980af461c8efdf36f2f3955
MoriMokata <b6214562@g.sut.ac.th>
2022-01-26 22:48:08 +0700

    "close #10 add Facility"

--- backend/entity/Facility.go
+++ backend/entity/Facility.go
@@ -1,13 +1,32 @@
 package entity
 
 import (
-
+	"time"
 	"gorm.io/gorm"
 )
 
 type Package struct {
 	gorm.Model
 	Name string
-	// Facility []Facility `gorm:"foreignKey:PackageID"`
+	Facility []Facility `gorm:"foreignKey:PackageID"`
 
+}
+
+
+type Facility struct {
+	gorm.Model
+
+	UserID *uint
+	User   User
+
+	No          string
+	PackageTime time.Time
+	Price       int
+	Confirm     bool
+
+	PackageID *uint
+	Package   Package
+
+	// TrainnerID *uint
+	// Trainner   Trainner
 }
