0b147cf07c718e9713642b06033fb65f484b935c
MoriMokata <b6214562@g.sut.ac.th>
2022-01-26 22:39:37 +0700

    "close #8 add user"

---
+++ backend/entity/user.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string
+	Email    string
+	Password string
+	Tel      string
+	Gender   string
+	Brithday time.Time
+
+	// Facility []Facility `gorm:"foreignKey:UserID"`
+
+	// RoleID *uint
+	// Role Role
+}
